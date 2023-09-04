a2a6585d4034ea18b0e14f649ceb85b6235ce2f5
wet toast <137027047+ngw4@users.noreply.github.com>
2023-09-04 17:17:16 +1200

    uploaded index.js and index.css

---
+++ index.js
@@ -0,0 +1,32 @@
+// setup IntersectionObserver
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        window.console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+// get all the elements to be observed
+const targets = document.querySelectorAll('.hidden');
+
+// start observing
+targets.forEach((target) => {
+    observer.observe(target);
+});
+
+//daddy github copilot wrote comments for me :3
+
+const text = document.querySelectorAll('#ngw4text');
+
+const textArray = ['____', 'n___', 'ng__', 'ngw_', 'ngw4']
+
+const typeWriter = (textArray, element, speed) => {
+    textArray.forEach((word, i) => {
+        setTimeout(() => (element.innerHTML = word), speed * i);
+    });
+};
+// wait 1 second and then start typewriter
+
+typeWriter(textArray, text[0], 430)
